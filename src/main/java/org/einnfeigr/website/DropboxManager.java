9efcb59fe157f9b3e671bb1e245f79ebf99c6b91
studiedlist <studiedlist@localhost>
2021-02-07 12:41:16 +0300

    add readFolder method and change environmental param name

--- src/main/java/org/einnfeigr/website/DropboxManager.java
+++ src/main/java/org/einnfeigr/website/DropboxManager.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,8 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.DownloadErrorException;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
 
 @Component
 public class DropboxManager {
@@ -19,7 +23,7 @@ public class DropboxManager {
 
 	public DropboxManager() {
 		DbxRequestConfig config = DbxRequestConfig.newBuilder("einnfeigr website").build();
-		client = new DbxClientV2(config, System.getenv("dropbox.access_token"));
+		client = new DbxClientV2(config, System.getenv("DROPBOX_ACCESS_TOKEN"));
 	}
 	
 	public byte[] readFileContent(String path) 
@@ -32,6 +36,25 @@ public class DropboxManager {
 			} else {
 				throw e;
 			}
+		}
+	}
+	
+	public List<Metadata> readFolder(String path) throws DbxException {
+		try {
+			List<ListFolderResult> results = new ArrayList<>();
+			ListFolderResult result = client.files().listFolder(path);
+			results.add(result);
+			while(result.getHasMore()) {
+				result = client.files().listFolderContinue(result.getCursor());
+				results.add(result);
+			} 
+			List<Metadata> metadata = new ArrayList<>();
+			results.forEach((r) -> {
+				metadata.addAll(r.getEntries());
+			});
+			return metadata;
+		} catch (DbxException e) {
+			throw e;
 		}
 	}
 	
