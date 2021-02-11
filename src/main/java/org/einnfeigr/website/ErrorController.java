9ad544bb485916d6dbe068e80f49a906d1de4f75
studiedlist <studiedlist@localhost>
2021-02-11 20:54:55 +0300

    rename error page template

--- src/main/java/org/einnfeigr/website/ErrorController.java
+++ src/main/java/org/einnfeigr/website/ErrorController.java
@@ -2,17 +2,12 @@ package org.einnfeigr.website;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Locale;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mobile.device.Device;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,10 +21,10 @@ public class ErrorController extends ResponseEntityExceptionHandler {
 	private final static Logger log = LoggerFactory.getLogger(ErrorController.class);
 	
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<Object> handle(Exception e, ControllerUtils builder)
+	public ModelAndView handle(Exception e, ControllerUtils builder)
 			throws IOException, ServletException {
 		log.info("Handler resolved exception", e);
-		String path = "error";
+		String path = "err";
 		boolean error404;
 		HttpStatus responseStatus;
 		if(e instanceof FileNotFoundException || e instanceof NoHandlerFoundException) {
@@ -39,8 +34,7 @@ public class ErrorController extends ResponseEntityExceptionHandler {
 			responseStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 			error404 = false;
 		}
-		return new ResponseEntity<Object>(
-				builder.buildMav(path, responseStatus, "error.404", error404), responseStatus);
+		return builder.buildMav(path, responseStatus, "error.404", error404);
 	}
 
 	
