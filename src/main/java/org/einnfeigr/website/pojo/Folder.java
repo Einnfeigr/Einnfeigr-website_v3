4e4e8be6ced7f95df7f055b3105bb0ee90da40c5
studiedlist <studiedlist@localhost>
2021-02-16 21:15:33 +0300

    add getters and setters to Folder class

--- src/main/java/org/einnfeigr/website/pojo/Folder.java
+++ src/main/java/org/einnfeigr/website/pojo/Folder.java
@@ -5,16 +5,18 @@ import java.util.List;
 
 public class Folder {
 
-	private List<String> files = new ArrayList<>();
+	private List<Note> notes = new ArrayList<>();
 	private List<Folder> folders = new ArrayList<>();
 	private String path;
+	private String name;
 	
-	public List<String> getFiles() {
-		return files;
+	public Folder() {}
+	
+	public Folder(File file) {
+		this.path = file.getPath();
+		this.name = path.substring(path.lastIndexOf("/")+1);
 	}
-	public void setFiles(List<String> files) {
-		this.files = files;
-	}
+	
 	public List<Folder> getFolders() {
 		return folders;
 	}
@@ -27,5 +29,22 @@ public class Folder {
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
+	public List<Note> getNotes() {
+		return notes;
+	}
+	public void setNotes(List<Note> notes) {
+		this.notes = notes;
+	}
+	public void addNote(Note note) {
+		notes.add(note);
+	}
+	public void addFolder(Folder folder) {
+		folders.add(folder);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
