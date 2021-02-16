7b774d2fcb5fcaf2886f2e7d08c523f721f745f3
studiedlist <studiedlist@localhost>
2021-02-16 21:15:46 +0300

    update tests

--- src/test/java/org/einnfeigr/website/FileMarkdownHelperTests.java
+++ src/test/java/org/einnfeigr/website/FileMarkdownHelperTests.java
@@ -8,17 +8,23 @@ import java.util.Map;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
 
+@DisplayName("FileMarkdownHelper class tests")
 public class FileMarkdownHelperTests {
 
 	private static Handlebars hbs = new Handlebars();
 	private final static String HELPER_PATTERN = "{{md \"%s\" }}";
 	private Template template;
 	private Map<String, String> params = new HashMap<>();
+	private final static Parser parser = Parser.builder().build();
+	private final static HtmlRenderer renderer = HtmlRenderer.builder().build();
 	
 	@BeforeAll
 	static void initHbs() {
@@ -30,49 +36,55 @@ public class FileMarkdownHelperTests {
 		params.put("locale", "en");
 	}
 	
+	@DisplayName("Compile simple file")
 	@Test
 	void testCompile() throws IOException {
-		String output = "<h1>Test</h1>";
+		String output = renderer.render(parser.parse("# Test"));
 		template = hbs.compileInline(String.format(HELPER_PATTERN, "test"));
 		assertEquals(output, template.apply(params));	
 	}
 	
+	@DisplayName("Try to compile non existing file")
 	@Test
 	void testInvalidPath() throws IOException {
 		template = hbs.compileInline(String.format(HELPER_PATTERN, "missing"));
 		assertEquals("", template.apply(params));
 	}
 	
+	@DisplayName("Try to compile without locale")
 	@Test
 	void testNoLocale() throws IOException {
 		template = hbs.compileInline(String.format(HELPER_PATTERN, "test"));
 		assertEquals("", template.apply(null));
 	}
 	
+	@DisplayName("Test path formatting")
 	@Test
 	void testPathFormatting() throws IOException {
-		String output = "<h1>Test</h1>";
+		String output = renderer.render(parser.parse("# Test"));
 		String[] names = new String[] { "test.md", "/test", "test.txt", "/test.txt" };
 		for(String name : names) {
 			template = hbs.compileInline(String.format(HELPER_PATTERN, name));
-			assertEquals(output, template.apply(params));
+			assertEquals(output, template.apply(params));	
 		}
 	}
 	
+	@DisplayName("Test locale setting")
 	@Test
 	void testLocale() throws IOException {
-		String outputEn = "<h1>Test</h1>";
-		String outputRu = "<h1>Тест</h1>";
+		String outputEn = renderer.render(parser.parse("# Test"));
+		String outputRu = renderer.render(parser.parse("# Тест"));
 		template = hbs.compileInline(String.format(HELPER_PATTERN, "test"));
 		params.put("locale", "en");
 		assertEquals(outputEn, template.apply(params));	
 		params.put("locale", "ru");
-		assertEquals(outputRu, template.apply(params));			
+		assertEquals(outputRu, template.apply(params));	
 	}
 	
+	@DisplayName("Compile Handlebars inside Markdown")
 	@Test
 	void testHandlebarsInMarkdown() throws IOException {
-		String output = "<h1>Test</h1>";
+		String output = renderer.render(parser.parse("# Test"));
 		String text = "{{md \"testhbs.md\" }}";
 		params.put("text", "Test");
 		assertEquals(output, hbs.compileInline(text).apply(params));
--- src/test/java/org/einnfeigr/website/WebsiteApplicationTests.java
+++ src/test/java/org/einnfeigr/website/WebsiteApplicationTests.java
@@ -3,19 +3,23 @@ package org.einnfeigr.website;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.Cookie;
 
 import org.assertj.core.util.Lists;
-import org.einnfeigr.website.controller.view.MediaController;
+import org.einnfeigr.website.manager.DropboxManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.pegdown.PegDownProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +27,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
 
 
 @SpringBootTest
@@ -39,6 +47,9 @@ class WebsiteApplicationTests {
 	
 	private final static String HOME_ADDRESS = "/";
 	private final static String MOBILE_CHECK_TEXT = "<meta name=\"viewport";
+	private final static Parser parser = Parser.builder().build();
+	private final static HtmlRenderer renderer = HtmlRenderer.builder().build();
+	private final static Logger log = LoggerFactory.getLogger(WebsiteApplicationTests.class);
 	
 	public final static String MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V " 
 			+ "Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile " 
@@ -155,14 +166,8 @@ class WebsiteApplicationTests {
 	
 	@Test
 	void downloadMedia() throws Exception {
-		MediaController.CACHE_AGE_SECONDS = 1;
-		for(int x = 0; x < 3; x++) { 
-			mvc.perform(MockMvcRequestBuilders.get("/fridrum/media/media/test.jpg"))
-				.andExpect(MockMvcResultMatchers.status().isOk());
-			if(x == 2) {
-				Thread.sleep(6000);
-			}
-		}
+		mvc.perform(MockMvcRequestBuilders.get("/fridrum/media/media/test.jpg"))
+			.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 	
 	@Test
@@ -180,18 +185,49 @@ class WebsiteApplicationTests {
 	@Test
 	void createAlbum() throws Exception {
 		try {
+			mvc.perform(MockMvcRequestBuilders.get("/fridrum/albums/test"+hash))
+				.andExpect(MockMvcResultMatchers.status().isNotFound());
 			dropboxManager.createFolder("albums/test"+hash);
 			mvc.perform(MockMvcRequestBuilders.get("/fridrum/albums/test"+hash))
 				.andExpect(MockMvcResultMatchers.status().isOk());
 		} finally {
 			dropboxManager.delete("albums/test"+hash);
 			mvc.perform(MockMvcRequestBuilders.get("/fridrum/albums/test"+hash))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+			mvc.perform(MockMvcRequestBuilders.post("/api/cache/reset")
+					.with(user("admin").roles("admin")))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+			mvc.perform(MockMvcRequestBuilders.get("/fridrum/albums/test"+hash))
 				.andExpect(MockMvcResultMatchers.status().isNotFound());
 		}
 	}
 
 	@Test
-	void createLocalizedNote() throws Exception {
+	void createLocalizedAlbum() throws Exception {
+		String path = "albums/test"+hash;
+		Map<String, String> data = new HashMap<>();
+		data.put("ru", "тест");
+		data.put("en", "test");
+		ObjectMapper mapper = new ObjectMapper();
+		byte[] bytes = mapper.writeValueAsBytes(data);
+		try {
+			mvc.perform(MockMvcRequestBuilders.get("/fridrum/"+path))
+				.andExpect(MockMvcResultMatchers.status().isNotFound());
+			dropboxManager.createFolder(path);
+			dropboxManager.writeFile(path+"/locales.properties", bytes);
+			mvc.perform(MockMvcRequestBuilders.get("/fridrum/"+path))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string(containsString("test")));
+			mvc.perform(MockMvcRequestBuilders.get("/fridrum/"+path).param("lang", "ru"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string(containsString("тест")));
+		} finally {
+			dropboxManager.delete(path);
+		}
+	}
+	
+	@Test
+	void createNotes() throws Exception {
 		String filename = "test"+hash;
 		String path = "notes/%s/%s.md";
 		createAndTestNote(filename, String.format(path, "en", filename), "# Test", "en");
@@ -199,16 +235,26 @@ class WebsiteApplicationTests {
 	}
 	
 	void createAndTestNote(String name, String path, String content, String lang) throws Exception {
-		String output = new PegDownProcessor().markdownToHtml(content);
+		String output = renderer.render(parser.parse(content));
 		try {
 			mvc.perform(MockMvcRequestBuilders.get("/fridrum/notes/"+name).param("lang", lang))
 				.andExpect(MockMvcResultMatchers.status().isNotFound());			
-			dropboxManager.writeFileContent(path, content.getBytes());
+			dropboxManager.writeFile(path, content.getBytes());
 			mvc.perform(MockMvcRequestBuilders.get("/fridrum/notes/"+name).param("lang", lang))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string(containsString(output)));
+			mvc.perform(MockMvcRequestBuilders.get("/fridrum/notes/"+name+".md")
+					.param("lang", lang))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.content().string(containsString(output)));
 		} finally {
 			dropboxManager.delete(path);
+			mvc.perform(MockMvcRequestBuilders.get("/fridrum/notes/"+name).param("lang", lang))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string(containsString(output)));
+			mvc.perform(MockMvcRequestBuilders.post("/api/cache/reset")
+					.with(user("admin").roles("admin")))
+				.andExpect(MockMvcResultMatchers.status().isOk());
 			mvc.perform(MockMvcRequestBuilders.get("/fridrum/notes/"+name).param("lang", lang))
 				.andExpect(MockMvcResultMatchers.status().isNotFound());			
 		}
